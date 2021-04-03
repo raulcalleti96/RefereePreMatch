d66cc09b8e2809acc8363b68ecd691a7fe602b03
raulcalleti96 <raulcalleti96@icloud.com>
2021-04-04 00:38:18 +0200

    Design main Menu for referees

--- app/src/main/java/com/rsp/refereeprematch/Login.java
+++ app/src/main/java/com/rsp/refereeprematch/Login.java
@@ -1,13 +1,35 @@
 package com.rsp.refereeprematch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity
+{
 
+    private Button buttonLogin;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginscreen);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent (Login.this, MenuPrincipalArbiter.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
---
+++ app/src/main/java/com/rsp/refereeprematch/MenuPrincipalArbiter.java
@@ -0,0 +1,24 @@
+package com.rsp.refereeprematch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MenuPrincipalArbiter extends AppCompatActivity {
+
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menuprincipalarbitro);
+
+    }
+
+
+}
